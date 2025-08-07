3f73a2cb361586c2987e6e2167ca61d87e136123
priyasini <priyasinipandey7@gmail.com>
2025-08-07 16:44:50 +0530

    Arithmetic.java

---
+++ Arithmetic.java
@@ -0,0 +1,12 @@
+class Arithmetic
+{
+public static void main(String[]args)
+{
+  int v1=45,v2=5,v3=3;
+  System.out.println("addition of two values(v1+v2):"+(v1+v2));
+  System.out.println("substraction of two values(v1-v2):"+(v1-v2));
+  System.out.println("multiplication of two values(v1*v3):"+(v1*v3));
+  System.out.println("division of two values(v1/v2):"+(v1/v2));
+  System.out.println("mod division of two values(v1%v2):"+(v1%v2));
+}
+}
